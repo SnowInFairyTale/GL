bc9ca69bafed198b53d007659f97be309934a8ac
guozhipeng <guozhipeng@megvii.com>
2025-11-09 13:56:04 +0800

    。。

---
+++ app/src/main/java/com/example/gl/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.gl;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.sd_bt).setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TerrainGLActivity.class);
+            startActivity(intent);
+        });
+        findViewById(R.id.zy_bt).setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, GLActivity.class);
+            startActivity(intent);
+        });
+    }
+}
